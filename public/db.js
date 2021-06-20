6ed6e47722117c38a86a0172a6c1eb485e83e96b
Lisa Barton <lisabarton23@yahoo.com>
2021-06-20 13:27:53 -0600

    added indexs tp budgetSTore

--- public/db.js
+++ public/db.js
@@ -1,8 +1,28 @@
+
+let db;
+let budgetStore;
+
 const request = indexedDB.open ("BudgetDB, 1")
 
 request.onupgradeneeded = function (e) {
+db = e.target.result;
+
+if (db.objectStoreNames.length === 0) {
+    budgetStore = 
+db.createObjectStore ('budget', {autoIncrement: true});
+
+}
+
+ budgetStore.createIndex("dateIndex", "date");
+ budgetStore.createIndex("nameIndex", "name");
+ budgetStore.createIndex("valueIndex", "value");
+ 
 
 
 
-    
+}
+request.onsuccess = function (e) {
+
+
+
 }
