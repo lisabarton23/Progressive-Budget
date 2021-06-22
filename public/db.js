f773f53ce5521f74e8982619b2454a7da63803d5
Lisa Barton <lisabarton23@yahoo.com>
2021-06-21 20:01:49 -0600

    recoded service worker and db.js with, changed valueIndex should be working

--- public/db.js
+++ public/db.js
@@ -1,6 +1,6 @@
 
 let db;
-let budgetStore;
+// let budgetStore;
 
 const request = indexedDB.open ("BudgetDB, 1")
 
@@ -9,13 +9,13 @@ db = e.target.result;
 
 if (db.objectStoreNames.length === 0) {
     budgetStore = 
-db.createObjectStore ('budget', {autoIncrement: true});
+db.createObjectStore ('valueIndex', {autoIncrement: true});
 
 }
 
- budgetStore.createIndex("dateIndex", "date");
- budgetStore.createIndex("nameIndex", "name");
- budgetStore.createIndex("valueIndex", "value");
+//  budgetStore.createIndex("dateIndex", "date");
+//  budgetStore.createIndex("nameIndex", "name");
+ //budgetStore.createIndex("valueIndex", "value");
  
 
 
@@ -25,10 +25,74 @@ request.onerror = function (e) {
     console.log(`Woops! ${e.target.errorCode}`);
   };
   
-request.onsuccess = function (e) {
-
-    const transaction = db.transaction(['budgetStore'], 'readwrite')
-
-    .add({name: "job deposit", value: '1000', date: '2/02/2021'})
-
-}
+//   
+  
+function checkDatabase() {
+    console.log('check db invoked');
+  
+    // Open a transaction on your BudgetStore db
+    let transaction = db.transaction(['valueIndex'], 'readwrite');
+  
+    // access your BudgetStore object
+    const store = transaction.objectStore('valueIndex');
+  
+    // Get all records from store and set to a variable
+    const getAll = store.getAll();
+  
+    // If the request was successful
+    getAll.onsuccess = function () {
+      // If there are items in the store, we need to bulk add them when we are back online
+      if (getAll.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => response.json())
+          .then((res) => {
+            // If our returned response is not empty
+            if (res.length !== 0) {
+              // Open another transaction to BudgetStore with the ability to read and write
+              transaction = db.transaction(['BudgetStore'], 'readwrite');
+  
+              // Assign the current store to a variable
+              const currentStore = transaction.objectStore('BudgetStore');
+  
+              // Clear existing entries because our bulk add was successful
+              currentStore.clear();
+              console.log('Clearing store 🧹');
+            }
+          });
+      }
+    };
+  }
+  
+  request.onsuccess = function (e) {
+    console.log('success');
+    db = e.target.result;
+  
+    // Check if app is online before reading from db
+    if (navigator.onLine) {
+      console.log('Backend online! 🗄️');
+      checkDatabase();
+    }
+  };
+  
+  const saveRecord = (record) => {
+    console.log('Save record invoked');
+    // Create a transaction on the BudgetStore db with readwrite access
+    const transaction = db.transaction(['valueIndex'], 'readwrite');
+  
+    // Access your BudgetStore object store
+    const store = transaction.objectStore('valueIndex');
+  
+    // Add record to your store with add method.
+    store.add(record);
+  };
+  
+  // Listen for app coming back online
+  window.addEventListener('online', checkDatabase);
+  
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 // 
 //This is where I want to be able to save the offline info
-const CACHE_NAME ="Static-cache-v2";
-const DATA_CACHE_NAME ="data-cache-v1";
+// const CACHE_NAME ="Static-cache-v2";
+// const DATA_CACHE_NAME ="data-cache-v1";
 const FILES_TO_CACHE = [
     "/db.js",
     "/index.html",
@@ -10,68 +10,70 @@ const FILES_TO_CACHE = [
     "/service-worker.js",
     "/styles.css",
     "/",
-    "./icons/icon-192x192.png",
-    "./icons/icon-512x512.png"
-
-
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 
 ]
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
-self.addEventListener("install", function (evt) {
-    evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/icons"))
-        );
-        evt.waitUntil(
-            caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-          );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+// self.addEventListener("install", function (evt) {
+//     evt.waitUntil(
+//         caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api"))
+//         );
+//         evt.waitUntil(
+//             caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+//           );
       
-          // tell the browser to activate this service worker immediately once it
-          // has finished installing
-          self.skipWaiting();
-        });
-        self.addEventListener("activate", function(evt) {
-            evt.waitUntil(
-              caches.keys().then(keyList => {
-                return Promise.all(
-                  keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                      console.log("Removing old cache data", key);
-                      return caches.delete(key);
-                    }
-                  })
-                );
-              })
-            );
-        
-            self.clients.claim();
-          });
-
-        self.addEventListener('fetch', function(evt) {
-            // code to handle requests goes here
-            if (evt.request.url.includes("/api"))   {
-            evt.respondWith(
-                caches.open(DATA_CACHE_NAME).then (cache => {
-                    return fetch(evt.request)
-                    .then (response => {
-                        if (response.status ===200) {
-                            cache.put(evt.request.url, response.clone());
-                        }
-                        return response
-                    })
-                    .catch (err =>{
-                        return cache.match (evt.request)
-
-                    });
-                }).catch (err => console.log(err))
-            )
-            return;
-
-            }
+//           // tell the browser to activate this service worker immediately once it
+//           // has finished installing
+//           self.skipWaiting();
+//         });
+self.addEventListener('activate', (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+      caches
+        .keys()
+        .then((cacheNames) => {
+          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+        })
+        .then((cachesToDelete) => {
+          return Promise.all(
+            cachesToDelete.map((cacheToDelete) => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+  
+          return caches.open(RUNTIME).then((cache) => {
+            return fetch(event.request).then((response) => {
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
             });
-            evt.respondWith(
-                caches.open(CACHE_NAME).then(cache => {
-                  return cache.match(evt.request).then(response => {
-                    return response || fetch(evt.request);
-                  });
-                })
-              );
+          });
+        })
+      );
+    }
+  });
+  
